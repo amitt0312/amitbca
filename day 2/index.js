50a691f24dfeb6bcceb19cddf9e847e73dc75ba5
Dhruv6616 <dr1249114@gmail.com>
2024-10-17 14:06:52 +0530

    first commit

---
+++ day 2/index.js
@@ -0,0 +1,48 @@
+// function fun(){
+//     window.alert("Hello wld!");console.log("hiii");
+// }
+// fun();
+// document.write("Oii")
+// document.write("Luffy")
+// let v = 5
+// v = 8
+// console.log(v)
+
+// hey =40
+// console.log(typeof(hey))
+
+// const k = 10
+// k =30
+// console.log(k)
+
+
+// for (let i = 0; i <=10; i++) {
+//     console.log("i=",i)
+    
+// }
+// let i=0;
+// while (i===0){
+//     console.log(" i'm while loop")
+// }
+let array=[1,2,3,4,5];
+let find=3;
+let l=false;
+for(let i=0;i<3;i++){
+    if(find===array[i])
+        l=true
+}
+// console.log(array)
+// console.log(typeof(array));
+// console.log(typeof(array[2]));
+// let b=array;
+// for(b=0;b<3;b++){
+//     console.log(b[array]);
+// }
+// const arr=[0,"BCA","Apple",true,false];
+// console.log(typeof(arr[0]));
+// if(5==5){
+//     const i=5
+// }
+// if(true){
+//     const i=8;
+// }
